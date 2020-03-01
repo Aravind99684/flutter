0d7cb240f5e4bd920c821be3246763e158bf709a
Aravind99684 <61409670+Aravind99684@users.noreply.github.com>
2020-03-01 10:41:34 +0530

    Add files via upload

---
+++ app_bar_widget/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+// app bar with multiple icons.
+void main(List<String> args) {
+  
+}
+
+class AppBarWidget extends StatelessWidget {
+  @override 
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: new Text("Title"),
+        actions: <Widget>[
+          new IconButton(
+            icon:new Icon(Icons.search),
+            onPressed: () {} ,
+            ),
+          new IconButton(
+            icon: new Icon(Icons.add),
+             onPressed: () {},
+             ),
+        ]
+      ),
+    );
+  }
+}
