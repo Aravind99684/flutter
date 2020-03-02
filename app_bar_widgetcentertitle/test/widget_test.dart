b12cc89873795e8d5c0bb2403d553958d14bcabd
Aravind99684 <61409670+Aravind99684@users.noreply.github.com>
2020-03-02 11:35:04 +0530

    Add files via upload

--- app_bar_widgetcentertitle/test/widget_test.dart
+++ app_bar_widgetcentertitle/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:app_bar_widgetcentertitle/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(App());
+    await tester.pumpWidget(AppBarWidget());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
